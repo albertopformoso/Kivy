d3adca9ab87ee6d177ab30cffce08f8a897087cd
albertopformoso <albertopformoso@gmail.com>
2020-12-24 16:41:13 -0600

    Intro to kvlang

--- source/0007_kvlang/main.py
+++ source/0007_kvlang/main.py
@@ -2,13 +2,10 @@
 # coding UTF-8
 
 from kivy.app import App
-from kivy.uix.label import Label
 
 
 class HelloApp(App):
-
-    def build(self):
-        return Label(text='Hello World')
+    pass
 
 
-HelloApp.run()
+HelloApp().run()
